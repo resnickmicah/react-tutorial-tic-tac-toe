e8f68e8b3c9f12e7c002357161f319e713cd6aa1
Micah Resnick <resnickmicah@gmail.com>
2024-08-07 15:22:20 -0400

    Evaluate next player and winner

--- src/App.js
+++ src/App.js
@@ -12,8 +12,12 @@ export default function Board() {
   const [xIsNext, setXINext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  const winner = calculateWinner(squares);
+  const nextPlayer = xIsNext ? "X" : "O";
+  const status = winner ? "Winner: " + winner : "Next player: " + nextPlayer;
+
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -23,6 +27,7 @@ export default function Board() {
   }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -40,4 +45,31 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // for each row in the above array
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    // if the state array at the index of the first column of the line array in the line is non-null
+    // and the letter in the first column matches the letter in the second and third columns of the line array
+    // e.g. if i == 0, the 0th square is non-null (e.g. "X") and the squares at state array idxs 1 and 2 are also both "X".
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      // Return the letter of the winner, e.g. "X"
+      return squares[a];
+    }
+  }
+
+  return null;
 }
