48e4a24548c3c32f784f24ba93ffcaa21d6bf3da
Micah Resnick <resnickmicah@gmail.com>
2024-08-07 15:31:22 -0400

    Add Game component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXINext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -44,6 +44,19 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
 
